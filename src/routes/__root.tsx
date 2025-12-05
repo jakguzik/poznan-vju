1659396f551c09ea3bb82d83ced87a24903955d2
Kuba <kuba@hehe.studio>
2025-12-05 02:14:38 +0100

    fix: update android icons and force cache bust for favicons

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -59,22 +59,22 @@ export const Route = createRootRouteWithContext<{
             {
                 rel: "apple-touch-icon",
                 sizes: "180x180",
-                href: "/apple-touch-icon.png"
+                href: "/apple-touch-icon.png?v=2"
             },
             {
                 rel: "icon",
                 type: "image/png",
                 sizes: "32x32",
-                href: "/favicon-32x32.png"
+                href: "/favicon-32x32.png?v=2"
             },
             {
                 rel: "icon",
                 type: "image/png",
                 sizes: "16x16",
-                href: "/favicon-16x16.png"
+                href: "/favicon-16x16.png?v=2"
             },
-            { rel: "manifest", href: "/site.webmanifest", color: "#fffff" },
-            { rel: "icon", href: "/favicon.ico" }
+            { rel: "manifest", href: "/site.webmanifest?v=2", color: "#fffff" },
+            { rel: "icon", href: "/favicon.ico?v=2" }
         ]
     }),
     errorComponent: (props) => {
