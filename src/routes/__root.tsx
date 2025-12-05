8348955e9c1ff5d882d96931c46587d2967e8767
Kuba <kuba@hehe.studio>
2025-12-05 02:18:09 +0100

    fix: add new favicon to all required locations

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -59,22 +59,22 @@ export const Route = createRootRouteWithContext<{
             {
                 rel: "apple-touch-icon",
                 sizes: "180x180",
-                href: "/apple-touch-icon.png?v=2"
+                href: "/apple-touch-icon.png?v=3"
             },
             {
                 rel: "icon",
                 type: "image/png",
                 sizes: "32x32",
-                href: "/favicon-32x32.png?v=2"
+                href: "/favicon-32x32.png?v=3"
             },
             {
                 rel: "icon",
                 type: "image/png",
                 sizes: "16x16",
-                href: "/favicon-16x16.png?v=2"
+                href: "/favicon-16x16.png?v=3"
             },
-            { rel: "manifest", href: "/site.webmanifest?v=2", color: "#fffff" },
-            { rel: "icon", href: "/favicon.ico?v=2" }
+            { rel: "manifest", href: "/site.webmanifest?v=3", color: "#fffff" },
+            { rel: "icon", href: "/favicon.ico?v=3" }
         ]
     }),
     errorComponent: (props) => {
